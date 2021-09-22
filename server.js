6aa5b124aa5a5c3e8c016d5bed53040125d0cb8c
madhavmahesh1969 <madhavmahesh@gmail.com>
2021-09-22 18:28:50 +0530

    udpated server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const ngApp = express();
 
-ngApp.use(express.static('./dist/angular-ionic-poc'));
+//ngApp.use(express.static('./dist/angular-ionic-poc'));
+ngApp.use(express.static('./www'));
 
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/angular-ionic-poc/index.html'));
+    //response.sendFile(path.join(__dirname, '/dist/angular-ionic-poc/index.html'));
+	response.sendFile(path.join(__dirname, '/www/index.html'));
 });
 
 ngApp.listen(process.env.PORT || 8080);
