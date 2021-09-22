64cdc0dad7b119ee7901eca8b109e0242743d496
madhavmahesh1969 <madhavmahesh@gmail.com>
2021-09-22 17:36:27 +0530

    udpated node version

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const ngApp = express();
 
-ngApp.use(express.static('./dist/angular-ionic'));
+ngApp.use(express.static('./dist/angular-ionic-poc'));
 
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
