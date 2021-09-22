1dea39eee66c49b8c0413e66e2a8be48e6822031
madhavmahesh1969 <madhavmahesh@gmail.com>
2021-09-22 17:22:23 +0530

    added server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const ngApp = express();
 
-ngApp.use(express.static('./dist/angular-forms-validation'));
+ngApp.use(express.static('./dist/angular-ionic'));
 
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
